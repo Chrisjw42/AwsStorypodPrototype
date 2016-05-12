6340b57ec99d43c2da3b3712c6d85c447485cb8f
Chrisjw42 <chrisjw42@gmail.com>
2016-05-12 15:36:50 +1000

    Table definition functioning

--- AwsStorypodPrototype/DatabaseConnection.cs
+++ AwsStorypodPrototype/DatabaseConnection.cs
@@ -15,7 +15,6 @@ namespace AwsConsoleApp1
         AmazonDynamoDBClient db;
         public DatabaseConnection()
         {
-            // db = new AmazonDynamoDBClient("AKIAJ65JLQAK4WBGS5NA", "EJDFXPwUb2HS90r0KGSA9UJDNFbRd7X9/Z0oWlXo");
             db = new AmazonDynamoDBClient();
         }
 
@@ -33,34 +32,110 @@ namespace AwsConsoleApp1
             Console.WriteLine("\nFinished!\n");
         }
 
-
-        public List<KeySchemaElement> CreateTableSchema()
+        /// <summary>
+        /// Create KeySchemaElemet list based on current attributes
+        /// </summary>
+        /// <param name="attributes"></param>
+        /// <returns></returns>
+        public List<KeySchemaElement> GetKeySchema(List<AttributeDefinition> attributes)
         {
-            string input = "";
-
-            do
-            {
-                // Name table
-                // loop add attributes 
-                input = "X";
-
-            } while (input.ToUpper() != "X");
-            Console.WriteLine("It's not ready yet, default schema created");
             List<KeySchemaElement> schema = new List<KeySchemaElement>();
 
-            KeySchemaElement schm = new KeySchemaElement("title", KeyType.HASH);
-
-            schema.Add(new KeySchemaElement("title", KeyType.HASH));
+            int counter = 0;
+            foreach (AttributeDefinition atr in attributes)
+            {
+                if (counter == 0)
+                {
+                    schema.Add(new KeySchemaElement(atr.AttributeName, KeyType.HASH));
+                }
+                else if (counter == 1)
+                {
+                    schema.Add(new KeySchemaElement(atr.AttributeName, KeyType.RANGE));
+                }
+                counter++;
+            }
             Console.WriteLine(schema.Count); 
             return schema;
         }
 
+        /// <summary>
+        /// Create a list of attributes for a new table.
+        /// </summary>
+        /// <returns></returns>
         public List<AttributeDefinition> GetAttributes()
         {
             List<AttributeDefinition> attributes = new List<AttributeDefinition>();
+            Console.WriteLine("---Let's define attribute attributes for the table---");
+            string input, inputType;
+            int numberOfAttributes = 0;
 
-            AttributeDefinition name = new AttributeDefinition("title", ScalarAttributeType.S);
-            attributes.Add(name);
+            do
+            {
+                // Clear input vars 
+                input = inputType = "";
+                Console.WriteLine("---Current attribute list:");
+                foreach (AttributeDefinition atr in attributes)
+                {
+                    Console.WriteLine("Name: {0} Type: {1}", atr.AttributeName, atr.AttributeType);
+                }
+                Console.WriteLine("---Please input new attribute name, or 'F' if finished:");
+                input = Console.ReadLine();
+
+                // If there's some kind of name given, that isn't an F
+                if (!(input == "" || input.ToUpper() == "F"))
+                {
+                    input = input.ToLower();
+                    Console.WriteLine("Thanks, cased attribute name will be: {0}", input);
+
+                    do
+                    {
+                        inputType = "";
+                        Console.WriteLine("Please define attribute type...\nB - Bool\nN - Number\nS - String");
+                        inputType = Console.ReadLine().ToUpper();
+
+                    } while (!(inputType == "B" || inputType == "N" || inputType == "S"));
+
+                    string inputConfirm = "";
+                    do
+                    {
+                        Console.WriteLine("Confirm addition of attribute (Y or N): \nName: {0} Type: {1}", input, inputType);
+                        inputConfirm = Console.ReadLine();
+                    } while (!(inputConfirm.ToUpper() == "Y" || inputConfirm.ToUpper() == "N"));
+
+                    // Finally, add new attribute to List is the user is happy
+                    if (inputConfirm.ToUpper() == "Y")
+                    {
+                        AttributeDefinition atrb = new AttributeDefinition();
+                        atrb.AttributeName = input;
+                        switch (inputType)
+                        {
+                            case "S":
+                                {
+                                    atrb.AttributeType = ScalarAttributeType.S;
+                                }
+                                break;
+                            case "N":
+                                {
+                                    atrb.AttributeType = ScalarAttributeType.N;
+                                }
+                                break;
+                            case "B":
+                                {
+                                    atrb.AttributeType = ScalarAttributeType.B;
+                                }
+                                break;
+                        }
+                        attributes.Add(atrb);
+                        numberOfAttributes++;
+                    }
+                }
+            } while (input.ToUpper() != "F" && numberOfAttributes < 2);
+
+            if (attributes.Count == 0)
+            {
+                Exception e = new Exception("You didnt define any attributes for your table!");
+            }
+            Console.WriteLine("Maximum number of schema attributes definted (2), creating table...");
             return attributes;
         }
 
@@ -75,13 +150,12 @@ namespace AwsConsoleApp1
                 }
 
                 // for readability
-                List<KeySchemaElement> proposedSchema = CreateTableSchema();
 
                 CreateTableRequest newTableReq = new CreateTableRequest();
                 // Instantiate an object before applying values.
                 newTableReq.TableName = proposedTableName;
                 newTableReq.AttributeDefinitions = GetAttributes();
-                newTableReq.KeySchema = proposedSchema;
+                newTableReq.KeySchema = GetKeySchema(newTableReq.AttributeDefinitions);
                 newTableReq.ProvisionedThroughput = new ProvisionedThroughput();
                 newTableReq.ProvisionedThroughput.ReadCapacityUnits = 1;
                 newTableReq.ProvisionedThroughput.WriteCapacityUnits = 1;
